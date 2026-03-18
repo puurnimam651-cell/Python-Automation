ef009cb9ffd3d5302e5f13ecb6e0ddde045cea03
puurnimam651-cell <puurnimam651@gmail.com>
2026-03-18 14:18:48 +0530

    Delete day1_print.py

--- day1_print.py
+++
@@ -1,8 +0,0 @@
-storage_devices = {
-    "Device A": "Healthy",
-    "Device B": "Warning: Low Space",
-    "Device C": "Critical: Offline"
-}
-
-for device, status in storage_devices.items():
-    print(f"{device}: {status}")
