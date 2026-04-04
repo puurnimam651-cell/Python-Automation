f960860a0d1b9e16f793fe6a75a5bea47e641931
puurnimam651-cell <puurnimam651@gmail.com>
2026-04-04 14:00:01 +0530

    Create lists.py

---
+++ day4_lists_basic/lists.py
@@ -0,0 +1,23 @@
+# Create a list
+colors = ["red", "green", "blue"]
+
+# Print the first item
+print(colors[0])
+
+# Change the second item to "yellow"
+colors[1] = "yellow"
+
+# Add "purple" to the end
+colors.append("purple")
+
+# Remove "red"
+colors.remove("red")
+
+# Print the list
+print(colors)
+
+
+Results
+
+red
+['yellow', 'blue', 'purple']
